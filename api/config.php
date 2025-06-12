989e45f3a2a403a976b45325eb8bb6394c31d07b
joelobros23 <laberintoj8@gmail.com>
2025-06-13 04:51:43 +0800

    feat: Set up the database connection (api/config.php).

--- api/config.php
+++ api/config.php
@@ -1,3 +1,15 @@
 <?php
-// PHP code will be generated here.
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "etherealechoes";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Set the character set to utf8mb4 to support emojis
+$conn->set_charset("utf8mb4");
 ?>
